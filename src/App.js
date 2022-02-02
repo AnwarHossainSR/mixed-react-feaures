19bf746105a2ce6cfbb8d19b3f108e1d594501fe
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-02 22:17:17 +0600

    React Loading Button With Spinner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "boxicons/css/boxicons.min.css";
+import { useState } from "react";
+import "./App.scss";
+import Button from "./components/button/Button";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button loading={loading} onClick={() => setLoading(!loading)}>
+        Loading
+      </Button>
     </div>
   );
 }
